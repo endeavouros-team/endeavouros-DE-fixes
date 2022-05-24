057b314fc8238a5ed1756060859e1480e8f94397
Johannes Kamprad <archlinux@kamprad.net>
2022-05-24 21:10:41 +0200

    Update set_once_plasma.sh
    a clown say that it is a bad idea to first remove the script that after removing itself try to run a command ;)

--- plasma/set_once_plasma.sh
+++ plasma/set_once_plasma.sh
@@ -2,5 +2,5 @@
 mkdir -p $HOME/.local/share/plasma/plasmoids
 cp -a /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager $HOME/.local/share/plasma/plasmoids/
 sed -i "s?      <default>applications:systemsettings.desktop,applications:org.kde.discover.desktop,preferred://filemanager,preferred://browser</default>?      <default>applications:systemsettings.desktop,applications:org.kde.konsole.desktop,preferred://filemanager,preferred://browser</default>?" "$HOME/.local/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml"
-rm ~/.config/autostart/set_once_plasma.desktop ~/set_once_plasma.sh
 kquitapp5 plasmashell || killall plasmashell && kstart5 plasmashell
+rm ~/.config/autostart/set_once_plasma.desktop ~/set_once_plasma.sh
