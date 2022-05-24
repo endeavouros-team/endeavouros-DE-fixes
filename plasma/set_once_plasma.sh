6b36a639c7a1936762275c10ab19f329dbb4df97
Johannes Kamprad <archlinux@kamprad.net>
2022-05-24 21:02:57 +0200

    Update set_once_plasma.sh
    fix script to use $HOME and reload plasma shell to apply changes

--- plasma/set_once_plasma.sh
+++ plasma/set_once_plasma.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-mkdir -p ~/.local/share/plasma/plasmoids
-cp -a /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager ~/.local/share/plasma/plasmoids/
-sed -i "s?      <default>applications:systemsettings.desktop,applications:org.kde.discover.desktop,preferred://filemanager,preferred://browser</default>?      <default>applications:systemsettings.desktop,applications:org.kde.konsole.desktop,preferred://filemanager,preferred://browser</default>?" "~/.local/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml"
+mkdir -p $HOME/.local/share/plasma/plasmoids
+cp -a /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager $HOME/.local/share/plasma/plasmoids/
+sed -i "s?      <default>applications:systemsettings.desktop,applications:org.kde.discover.desktop,preferred://filemanager,preferred://browser</default>?      <default>applications:systemsettings.desktop,applications:org.kde.konsole.desktop,preferred://filemanager,preferred://browser</default>?" "$HOME/.local/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml"
 rm ~/.config/autostart/set_once_plasma.desktop ~/set_once_plasma.sh
+kquitapp5 plasmashell || killall plasmashell && kstart5 plasmashell
