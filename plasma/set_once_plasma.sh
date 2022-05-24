ba95daaed844df6f65043c761be75b8be340a2b6
Johannes Kamprad <archlinux@kamprad.net>
2022-05-24 20:43:49 +0200

    Update set_once_plasma.sh

--- plasma/set_once_plasma.sh
+++ plasma/set_once_plasma.sh
@@ -2,5 +2,4 @@
 mkdir -p ~/.local/share/plasma/plasmoids
 cp -a /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager ~/.local/share/plasma/plasmoids/
 sed -i "s?      <default>applications:systemsettings.desktop,applications:org.kde.discover.desktop,preferred://filemanager,preferred://browser</default>?      <default>applications:systemsettings.desktop,applications:org.kde.konsole.desktop,preferred://filemanager,preferred://browser</default>?" "~/.local/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml"
-rm ~/.config/autostart/set_once_plasma.desktop
-rm ~/set_once_plasma.sh
+rm ~/.config/autostart/set_once_plasma.desktop ~/set_once_plasma.sh
