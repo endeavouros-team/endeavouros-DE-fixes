a8b975e9443fe866a186ac8b5965dee0fcdca5dc
Johannes Kamprad <archlinux@kamprad.net>
2022-05-31 00:25:00 +0200

    Update set_once_lxde.sh

--- lxde/set_once_lxde.sh
+++ lxde/set_once_lxde.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cp /etc/xdg/lxpanel/default/panels/panel $HOME/.config/lxpanel/LXDE/panels/panel
 patch -u $HOME/.config/lxpanel/LXDE/panels/panel -i $HOME/lxde.patch && lxpanelctl restart
 rm $HOME/.config/autostart/set_once_lxde.desktop $HOME/set_once_lxde.sh $HOME/lxde.patch
