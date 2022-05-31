3838cc4d85df4424cac66f6318a34da34c2106ec
Johannes Kamprad <archlinux@kamprad.net>
2022-05-31 13:44:07 +0200

    Update set_once_lxqt.sh
    fix echo scriptlet (i think i done fixing it)

--- lxqt/set_once_lxqt.sh
+++ lxqt/set_once_lxqt.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-echo "[quicklaunch]" >> "$HOME/.config/lxqt/panel.conf"
-echo "alignment=Left" >> "$HOME/.config/lxqt/panel.conf"
-echo "apps\1\desktop=/usr/share/applications/qterminal.desktop" >> "$HOME/.config/lxqt/panel.conf"
-echo "apps\2\desktop=/usr/share/applications/pcmanfm-qt.desktop" >> "$HOME/.config/lxqt/panel.conf"
-echo "apps\3\desktop=/usr/share/applications/firefox.desktop" >> "$HOME/.config/lxqt/panel.conf"
-echo "apps\size=3" >> "$HOME/.config/lxqt/panel.conf"
-echo "type=quicklaunch" >> "$HOME/.config/lxqt/panel.conf"
+
+cat <<EOT >> $HOME/.config/lxqt/panel.conf
+[quicklaunch]
+alignment=Left
+apps\1\desktop=/usr/share/applications/qterminal.desktop
+apps\2\desktop=/usr/share/applications/pcmanfm-qt.desktop
+apps\3\desktop=/usr/share/applications/firefox.desktop
+apps\size=3
+type=quicklaunch
+EOT
 
 killall lxqt-panel && lxqt-panel &
 rm $HOME/.config/autostart/set_once_lxde.desktop $HOME/set_once_lxde.sh
