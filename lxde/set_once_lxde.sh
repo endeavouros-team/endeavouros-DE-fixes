015c07413f6faf0c7c78e45e8792ee0a72119173
Johannes Kamprad <archlinux@kamprad.net>
2022-06-01 03:25:20 +0200

    Update set_once_lxde.sh

--- lxde/set_once_lxde.sh
+++ lxde/set_once_lxde.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cp /etc/xdg/lxpanel/default/panels/panel $HOME/.config/lxpanel/LXDE/panels/panel
-patch -u $HOME/.config/lxpanel/LXDE/panels/panel -i $HOME/lxde.patch && lxpanelctl restart
+sed -i "s?            id=gnome-terminal.desktop?            id=lxterminal.desktop?g" "$HOME/.config/lxpanel/LXDE/panels/panel"
+lxpanelctl restart &
 cp /etc/xdg/lxsession/LXDE/autostart $HOME/.config/lxsession/LXDE/autostart
-rm $HOME/.config/autostart/set_once_lxde.desktop $HOME/set_once_lxde.sh $HOME/lxde.patch
+rm $HOME/set_once_lxde.sh
