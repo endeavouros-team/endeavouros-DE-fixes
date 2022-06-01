fc94721166055e978b2bd0380bed8f5ea8958e77
Johannes Kamprad <archlinux@kamprad.net>
2022-06-01 13:54:00 +0200

    Delete set_once_lxde.sh
    not working

--- lxde/set_once_lxde.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cp /etc/xdg/lxpanel/default/panels/panel $HOME/.config/lxpanel/LXDE/panels/panel
-sed -i "s?            id=gnome-terminal.desktop?            id=lxterminal.desktop?g" "$HOME/.config/lxpanel/LXDE/panels/panel"
-sed -i "s?        image=/usr/share/lxpanel/images/my-computer.png?        image=/usr/share/lxde/images/lxde-icon.png?g" "$HOME/.config/lxpanel/LXDE/panels/panel"
-lxpanelctl restart &
-cp /etc/xdg/lxsession/LXDE/autostart $HOME/.config/lxsession/LXDE/autostart
-rm $HOME/set_once_lxde.sh
