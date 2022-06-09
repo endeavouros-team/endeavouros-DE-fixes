ea86a7c725038e3c3b7216a980ae127a576b93ba
Johannes Kamprad <archlinux@kamprad.net>
2022-06-09 15:22:32 +0200

    Update set_once_plasma.sh

--- plasma/set_once_plasma.sh
+++ plasma/set_once_plasma.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-sleep 4 &
 
-cat <<EOF >>$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc
-[Containments][2][Applets][5][Configuration][General]
-launchers=applications:systemsettings.desktop,applications:org.kde.konsole.desktop?wmClass=konsole,preferred://filemanager?wmClass=dolphin,preferred://browser?wmClass=firefox
-EOF
+kwriteconfig5 --file $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc --group "Containments" --group "2" --group "Applets" --group "5" --group "Configuration" --group "General" --key "launchers" "applications:systemsettings.desktop,applications:org.kde.konsole.desktop?wmClass=konsole,preferred://filemanager?wmClass=dolphin,preferred://browser?wmClass=firefox"
 
 kquitapp5 plasmashell || killall plasmashell && kstart5 plasmashell &
 rm ~/.config/autostart/set_once_plasma.desktop ~/set_once_plasma.sh
